597ee1f0b89c347f3bd75e1fa6d322978a283e77
Clement Venard <cvenard@gmail.com>
2018-03-13 10:59:40 +0800

    created a start server method to make debugging easier

--- main.go
+++ main.go
@@ -1,75 +1,84 @@
 package main
 
 import (
-	"log"
-	"io/ioutil"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 	"sync"
-	"github.com/gin-gonic/gin"
-	"github.com/go-yaml/yaml" 
+	// "github.com/gin-gonic/gin"
 	"time"
-)  
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-yaml/yaml"
+)
 
 // Config is a struct to parse the healthcheck yaml.
 // for go-yaml to work correctly all types need to be
 // exported otherwise it won't work.
 type Config struct {
 	Push []struct {
-		Name			  string 	`yaml:"name"`
-		URL 			  string 	`yaml:"url"`
-		Expression  string 	`yaml:"expression"`
+		Name       string `yaml:"name"`
+		URL        string `yaml:"url"`
+		Expression string `yaml:"expression"`
 	} `yaml:"push"`
 	Receive []struct {
-		Name 				string 	`yaml:"name"`
-		Message 		string 	`yaml:"message"`
-		Token 			string 	`yaml:"token"`
+		Name    string `yaml:"name"`
+		Message string `yaml:"message"`
+		Token   string `yaml:"token"`
 	} `yaml:"receive"`
 }
 
 // Health shows the status of our apps to anyone calling our healtcheck endpoint
 type Health struct {
 	Endpoints []struct {
-		Name 			string `json:"name"`
-		Status		string `json:"status"`
-		Message 	string `json:"message"`
+		Name    string `json:"name"`
+		Status  string `json:"status"`
+		Message string `json:"message"`
 	}
 	Jobs []struct {
-		Name 			string `json:"name"`
-		Status		string `json:"status"`
-		Message 	string `json:"message"`
-		Time 			string `json:"last_trigger"` 
+		Name    string `json:"name"`
+		Status  string `json:"status"`
+		Message string `json:"message"`
+		Time    string `json:"last_trigger"`
 	}
 }
 
 var err error
-var healthCheck []byte
 
+// UTILITY FUNCTIONS
 func checkError(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
 
-func unMarshalYaml (c *Config, path string) {
+var healthCheck []byte
+
+func unMarshalYaml(c *Config, path string) {
 	healthCheck, err = ioutil.ReadFile(path)
 	checkError(err)
-	err = yaml.Unmarshal([]byte(healthCheck), &c) 
+	err = yaml.Unmarshal([]byte(healthCheck), &c)
 	checkError(err)
 
 }
 
-func checkHealth (h *Health, c *Config) {
-
-	testEndpoints := func (wg *sync.WaitGroup, url string, name string) {
+func checkHealth(h *Health, c *Config) {
+	testEndpoints := func(wg *sync.WaitGroup, url string, name string) {
 		client := &http.Client{}
 		resp, err := client.Get(url)
 		checkError(err)
 		defer resp.Body.Close()
-		fmt.Printf("Name: %s\nURL: %s \nResponse Status: %v\nTime: %v\n", name, url, resp.Status, time.Now())
-		
-		// save response to a struct 
+		fmt.Printf("Name: %s\nURL: %s \nResponse Status: %v\nTime: %v\n",
+			name, url, resp.Status, time.Now())
 		defer wg.Done()
 	}
 
@@ -77,19 +86,12 @@ func checkHealth (h *Health, c *Config) {
 	wg.Add(len(c.Push))
 
 	for i := 0; i < len(c.Push); i++ {
-    go testEndpoints(&wg, c.Push[i].URL, c.Push[i].Name)
+		go testEndpoints(&wg, c.Push[i].URL, c.Push[i].Name)
 	}
-
 	wg.Wait()
 }
 
-var c Config
-var h Health
-
-func main() {
-
-	unMarshalYaml(&c, "./config.yml")
-		
+func startServer(port string) {
 	r := gin.Default()
 
 	r.GET("/healthcheck", func(g *gin.Context) {
@@ -97,5 +99,16 @@ func main() {
 		g.JSON(200, c.Push)
 	})
 
-	r.Run(":3333") // listen and serve on 0.0.0.0:3333
+	r.Run(port) // listen and serve on the port provided
+}
+
+var c Config
+var h Health
+
+func main() {
+	unMarshalYaml(&c, "./config.yml")
+	checkHealth(&h, &c)
+	port := getEnv("PORT", ":3333")
+	startServer(port)
+
 }
